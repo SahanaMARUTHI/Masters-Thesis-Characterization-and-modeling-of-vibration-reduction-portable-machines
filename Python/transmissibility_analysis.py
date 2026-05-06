350e6d4579f967fd842505ec1f44c2bcbf64b6e1
Sahana MARUTHI <mrt.sahana@gmail.com>
2026-05-06 17:31:33 +0200

    Add key interpretation print statement
    Added a print statement to display key interpretation of transmissibility analysis.

--- Python/transmissibility_analysis.py
+++ Python/transmissibility_analysis.py
@@ -1,1 +1,6 @@
-
+print("""
+Key Interpretation:
+- Peaks indicate resonance frequencies
+- High transmissibility (>1) = vibration amplification
+- Low transmissibility (<1) = isolation effectiveness
+""")
